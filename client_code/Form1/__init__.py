f4ae79ea0f2ef2575f41c8518af3352c87c83b05
guyscottages@gmail.com <guyscottages@gmail.com>
2025-05-17 08:14:57 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -3,7 +3,12 @@ from anvil import *
 
 class Form1(Form1Template):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    my_message = "This message is stored in the 'my_message' variable"
+    alert(my_message)
 
-    # Any code you write here will run before the form opens.
+  def button_1_click(self, **event_args):
+    my_message = 'You just clicked the button!'
+    alert(my_message)
+
+ 
