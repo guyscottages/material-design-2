2f4df5c90a6f64a0e3846f3303ffe63758a32907
guyscottages@gmail.com <guyscottages@gmail.com>
2025-05-18 09:09:23 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,11 +4,9 @@ from anvil import *
 class Form1(Form1Template):
   def __init__(self, **properties):
     self.init_components(**properties)
-    my_message = "This message is stored in the 'my_message' variable"
-    alert(my_message)
 
   def button_1_click(self, **event_args):
     my_message = 'You just clicked the button!'
-    alert(my_message)
+    alert(my_message) 
 
  
